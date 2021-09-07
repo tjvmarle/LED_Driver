a5ebdc2aa3598821e6ceb4c3fe9a3cf3f42110ae
tjvmarle <metalananas@gmail.com>
2021-09-07 20:12:09 +0200

    Initial commit

---
+++ LED Driver/src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+/*
+TODO:
+* Test each MOSFET with a small setup
+* Test the random() function with a couple of reboots
+* Make a nice transient random shuffle between different color (combinations)
+* Calibrate min/max values for potmeters
+*/
+
+// TODO: define relevant constants
+
+void setup()
+{
+  // put your setup code here, to run once:
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}
